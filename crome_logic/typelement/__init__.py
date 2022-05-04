e66f744a9e66a84d4bfaa43fb539c102859af265
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2022-05-03 17:32:31 -0700

    work in progress, TODO: fix deepcopy

---
+++ crome_logic/typelement/__init__.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from enum import Enum, auto
+from typing import TypeVar
+
+
+class TypeKind(Enum):
+    """Kinds of CromeType."""
+
+    UNKNOWN = auto()
+    SENSOR = auto()
+    LOCATION = auto()
+    ACTION = auto()
+    ACTIVE = auto()
+    CONTEXT = auto()
+    SENSOR_LOCATION = auto()
+    SENSOR_ACTION = auto()
+
+
+@dataclass(kw_only=True)
+class CromeType:
+    """Base class fo the types of CROME."""
+
+    name: str
+    kind: TypeKind = TypeKind.UNKNOWN
+    refinement_of: set[str] = field(init=False, default_factory=set)
+
+    def __str__(self):
+        return self.name
+
+    def __le__(self, other: CromeType):
+        return isinstance(self, type(other))
+
+    def __eq__(self, other):
+        if isinstance(other, CromeType):
+            if self.name == other.name and type(self).__name__ == type(other).__name__:
+                return True
+            return False
+        else:
+            raise AttributeError
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+AnyCromeType = TypeVar("AnyCromeType", bound=CromeType)
---
+++ crome_logic/typelement/robotic.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+from crome_logic.typelement import TypeKind
+from crome_logic.typelement.basic import (
+    BooleanControllable,
+    BooleanUncontrollable,
+    BoundedInteger,
+)
+
+
+@dataclass(kw_only=True)
+class BooleanAction(BooleanControllable):
+    kind: TypeKind = field(init=False, default=TypeKind.ACTION)
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+@dataclass(kw_only=True)
+class BooleanLocation(BooleanControllable):
+    kind: TypeKind = field(init=False, default=TypeKind.LOCATION)
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+@dataclass(kw_only=True)
+class BooleanSensor(BooleanUncontrollable):
+    kind: TypeKind = field(init=False, default=TypeKind.CONTEXT)
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+@dataclass(kw_only=True)
+class BooleanContext(BooleanUncontrollable):
+    kind: TypeKind = field(init=False, default=TypeKind.CONTEXT)
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+@dataclass(kw_only=True)
+class Active(BooleanControllable):
+    name: str = field(init=False, default="active")
+    kind = TypeKind.ACTIVE
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+@dataclass(kw_only=True)
+class IntegerAction(BoundedInteger):
+    kind: TypeKind = field(init=False, default=TypeKind.ACTION)
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
+
+
+@dataclass(kw_only=True)
+class IntegerSensor(BoundedInteger):
+    kind: TypeKind = field(init=False, default=TypeKind.SENSOR)
+
+    def __hash__(self):
+        return hash(self.name + type(self).__name__)
